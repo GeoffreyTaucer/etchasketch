2e3082d8206070fd30eb8ae6977aeee4cc1274fe
= <geoffreytaucer@gmail.com>
2019-06-06 14:13:53 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var container = document.getElementById('#container')
+var rows = []
+
+function createBoard(size) {
+    for (let i = 0; i < size; i++) {
+        var squares = []
+        for (let i = 0; i < size; i++) {
+            squares.push(document.createElement('span'))
+        }
+        rows.push(cols)
+    }
+    
+}

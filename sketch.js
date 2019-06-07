20058b8d6c0343d449c9cf4cef1f3e4926f8ca7f
Taucer <geoffreytaucer@gmail.com>
2019-06-07 13:49:00 -0400

    Add board creation functions that don't work

--- sketch.js
+++ sketch.js
@@ -1,13 +1,39 @@
-var container = document.getElementById('#container')
-var rows = []
+// var container = document.getElementById('#container')
+// var rows = []
 
-function createBoard(size) {
+// function createBoard(size) {
+//     for (let i = 0; i < size; i++) {
+//         var squares = []
+//         for (let i = 0; i < size; i++) {
+//             squares.push(document.createElement('span'))
+//         }
+//         rows.push(cols)
+//     }
+// }
+
+let containderdiv = document.getElementById('#board');
+
+function createRow (size) { // create a single row
+    let row = document.createElement('div');
+    let square = document.createElement('span');
+    square.style.height = '10px';
+    square.style.width = '10px';
+    square.style.border = '1px solid black'
+
     for (let i = 0; i < size; i++) {
-        var squares = []
-        for (let i = 0; i < size; i++) {
-            squares.push(document.createElement('span'))
-        }
-        rows.push(cols)
+        row.appendChild(square);
     }
-    
+    return row;
 }
+
+function createBoard(size) { // Stack rows
+    let row = createRow(size);
+
+    for (let i = 0; i < size; i++) {
+        containderdiv.appendChild(row);
+    }
+}
+
+const boardSize = parseInt(window.prompt("Enter board size", ""), 10); // get board size from user
+
+createBoard(boardSize)

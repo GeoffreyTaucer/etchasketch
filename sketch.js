59cfdee5aae43ffb63e478f9159cb600ff919597
= <geoffreytaucer@gmail.com>
2019-06-09 15:43:17 -0400

    Kind of works now

--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 const containderdiv = document.getElementById('board');
 
-function createRow (size) { // create a single row
+function createRow(size) { // create a single row
     let row = document.createElement('div');
-    let square = document.createElement('div');
-    square.style.height = '10px';
-    square.style.width = '10px';
-    square.style.border = '1px solid black'
-
+  
     for (let i = 0; i < size; i++) {
-        row.appendChild(square);
+      let square = document.createElement('span');
+      square.appendChild(document.createTextNode('_'))
+      square.style.height = '10px';
+      square.style.width = '10px';
+      square.style.border = '1px solid black'
+      row.appendChild(square);
     }
+    
     return row;
-}
+  }
 
 function createBoard(size) { // Stack rows
     for (let i = 0; i < size; i++) {

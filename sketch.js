a635be8753f056659bf9b6bdbd6e9c7e551178ce
Taucer <geoffreytaucer@gmail.com>
2019-06-23 11:37:50 -0400

    Functional 16x16 grid using cssgrid

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
-const containderdiv = document.getElementById('board');
-dims = getDim()
-containderdiv.style.width = dims
-containderdiv.style.height = dims
-
-function
-
-numSquares = 16;
+function CreateBoard(size) {
+  let boardDims = "repeat(" + size.toString() + ", 1fr)";
+  containerdiv.style.gridTemplate = boardDims + "/" + boardDims;
+  
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.color = "white";
+    square.style.border = "solid black 1px";
+    square.textContent = i.toString();
+    containerdiv.appendChild(square);
+  }
+}
 
 function GetDim () {
   let maxHeight = window.innerHeight;
@@ -18,3 +23,12 @@ function GetDim () {
   }
   return maxDim;
 }
+
+const containerdiv = document.getElementById('board');
+let sizeOfWindow = GetDim();
+containerdiv.style.width = sizeOfWindow.toString() + "px";
+containerdiv.style.height = sizeOfWindow.toString() + "px";
+
+let numSquares = 16;
+
+CreateBoard(numSquares);

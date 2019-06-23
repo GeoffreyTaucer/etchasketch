af2b865b4e3817bdcf7f716ffb68bb85ccbb97a5
Taucer <geoffreytaucer@gmail.com>
2019-06-23 12:18:03 -0400

    color squares black on mousover

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-function CreateBoard(size) {
+function CreateBoard(size, color) {
   let boardDims = "repeat(" + size.toString() + ", 1fr)";
   containerdiv.style.gridTemplate = boardDims + "/" + boardDims;
   
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    // square.id = "square" + i.toString;
     square.style.color = "white";
     square.style.border = "solid black 1px";
     square.textContent = i.toString();
+    square.addEventListener("mouseover", function() {colorSquare(square, color)});
     containerdiv.appendChild(square);
   }
 }
@@ -24,11 +26,17 @@ function GetDim () {
   return maxDim;
 }
 
+function colorSquare(square, color) {
+  square.style.color = color;
+  square.style.backgroundColor = color;
+}
+
 const containerdiv = document.getElementById('board');
 let sizeOfWindow = GetDim();
 containerdiv.style.width = sizeOfWindow.toString() + "px";
 containerdiv.style.height = sizeOfWindow.toString() + "px";
 
 let numSquares = 16;
+let color = "black"
 
-CreateBoard(numSquares);
+CreateBoard(numSquares, color);

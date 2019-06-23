f021f2024fc0e309d4cd8263c9a149cb0033787c
Taucer <geoffreytaucer@gmail.com>
2019-06-23 13:19:16 -0400

    Functional reset, layout changes

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function CreateBoard(size, color) {
+function CreateBoard(size, color = "black") {
   let boardDims = "repeat(" + size.toString() + ", 1fr)";
   containerdiv.style.gridTemplate = boardDims + "/" + boardDims;
   
@@ -21,7 +21,7 @@ function GetDim () {
   } else {
     maxDim = maxHeight
   }
-  return maxDim;
+  return maxDim - 2; // subtract two pixels, to allow for border
 }
 
 function colorSquare(square, color) {
@@ -29,10 +29,26 @@ function colorSquare(square, color) {
   square.style.backgroundColor = color;
 }
 
+function resetBoard() {
+  while (containerdiv.firstChild) {
+    containerdiv.removeChild(containerdiv.firstChild);
+  }
+  let boardRes = parseInt(prompt("Please enter integer value for board resolution"));
+  if (!boardRes) {
+    document.getElementById('status').textContent="No integet value given";
+    CreateBoard(16);
+  } else {
+    CreateBoard(boardRes);
+  }
+}
+
 const containerdiv = document.getElementById('board');
 let sizeOfWindow = GetDim();
 containerdiv.style.width = sizeOfWindow.toString() + "px";
 containerdiv.style.height = sizeOfWindow.toString() + "px";
+
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', resetBoard);
 
 let numSquares = 16;
 let color = "black"
